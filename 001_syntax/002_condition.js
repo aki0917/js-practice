e5d27f54ddb365f62796ddd0ad07f8941b148fbf
aki <akihirohigasa@AKIHIROnoMacBook-Pro.local>
2020-05-27 16:29:01 +0900

    003終了

--- 001_syntax/002_condition.js
+++ 001_syntax/002_condition.js
@@ -47,17 +47,17 @@ function greeding(hour) {
 function getDay(day) {
   if (day == 0) {
     console.log("Sunday");
-  } else if (day == 1) {
+  } else if (day === 1) {
     console.log("Monday");
-  } else if (day == 2) {
+  } else if (day ===  2) {
     console.log("Tuesday");
-  } else if (day == 3) {
+  } else if (day ===  3) {
     console.log("Wednesday");
-  } else if (day == 4) {
+  } else if (day ===  4) {
     console.log("Thursday");
-  } else if (day == 5) {
+  } else if (day ===  5) {
     console.log("Friday");
-  } else if (day == 6) {
+  } else if (day ===  6) {
     console.log("Saturday");
   } 
 }
--- 001_syntax/003_loop.js
+++ 001_syntax/003_loop.js
@@ -4,21 +4,30 @@
  */
 
 function printOneToTen() {
+  for(let i = -1; i < 10; i++) {
+    console.log(i+1);
+  }
 }
 
 /**
  *  3.2 0~10までの数字を数字の大きい順に表示するメソッドを実装してください
  *
  */
-
 function printOneToTenDesc() {
+  for(let i = 11; i > 0; i--) {
+    console.log(i-1);
+  }
 }
 
 /**
  *  3.3 while文を使って0~10までの数字を小さい順番に表示するメソッドを実装してください
  *
  */
-
 function printOneToTenWhile() {
+  var count = 0; 
+  while(count <= 10){
+    console.log(count);
+    count++;
+  }
 }
 
